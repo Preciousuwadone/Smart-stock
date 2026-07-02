dcc1eb60cf417c55931ef9f5a492c95d9a51e89c
Precious Uwadone <precious.uwadone@gmail.com>
2026-07-02 18:35:30 +0100

    Completed Nomba Integration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 from app.config import Config
 from app.extensions import db, jwt, bcrypt
 
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -14,7 +13,9 @@ def create_app():
     CORS(app)
 
     from app.auth.routes import auth_bp
+    from app.customers.routes import customers_bp
     app.register_blueprint(auth_bp)
+    app.register_blueprint(customers_bp)
 
     @app.route("/health")
     def health():
